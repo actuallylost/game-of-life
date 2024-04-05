dd5dfafc2aeb973f74fc8146bdba4e8739363481
Lost <pandagaminggr@gmail.com>
2024-04-05 19:18:19 +0300

    feat: add DefaultPlugins

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 use bevy_editor_pls::EditorPlugin;
 
+// EditorPlugin requires DefaultPlugins to work
 fn main() {
-    App::new().add_plugins(EditorPlugin::new()).run();
+    App::new()
+        .add_plugins((DefaultPlugins, EditorPlugin::default()))
+        .run();
 }
